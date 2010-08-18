b9fd1cfd192d056ea2f8e97dc41896d091b948b2
chenhenri@gmail.com <chenhenri@gmail.com@28855f3a-1600-e85e-1cf9-12e366649bb5>
2010-08-18 07:51:11 +0000

    1. Rewrite org.zkoss.spring.DelegatingVariableResolver to accept core/security VariableResolvers, so ZK Spring project does not depends on zkplus. 2. Make ZK Spring Java 1.5 dependent 3. Use Library properties instead of Configuration preferences.

--- zkspring-security/src/org/zkoss/spring/security/config/ZkDesktopReuseFilter.java
+++ zkspring-security/src/org/zkoss/spring/security/config/ZkDesktopReuseFilter.java
@@ -71,7 +71,6 @@ public class ZkDesktopReuseFilter extends GenericFilterBean implements
 
 	}
 	
-	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 		// TODO Auto-generated method stub
