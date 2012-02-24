1eee246b945aa8e858b4104c30d1e321ddb6ce9f
hawkchen@potix.com <hawkchen@potix.com@28855f3a-1600-e85e-1cf9-12e366649bb5>
2012-02-24 02:54:54 +0000

    initial.

---
+++ zkspringissue/src/main/java/org/zkoss/bind/examples/order/OrderVM.java
@@ -0,0 +1,100 @@
+/* OrderVM.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		2011/10/31 Created by Dennis Chen
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+ */
+package org.zkoss.bind.examples.order;
+
+import org.zkoss.bind.ValidationContext;
+import org.zkoss.bind.Validator;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.bind.validator.AbstractValidator;
+import org.zkoss.zul.ListModelList;
+
+/**
+ * @author dennis
+ * 
+ */
+public class OrderVM {
+
+	//the order list
+	ListModelList<Order> orders;
+	
+	//the selected order
+	Order selected;
+
+	public ListModelList<Order> getOrders() {
+		if (orders == null) {
+			//init the list
+			orders = new ListModelList<Order>(getService().list());
+		}
+		return orders;
+	}
+
+	public Order getSelected() {
+		return selected;
+	}
+
+	public void setSelected(Order selected) {
+		this.selected = selected;
+	}
+
+	//action command
+	
+	@NotifyChange({"selected","orders"})
+	@Command
+	public void newOrder(){
+		Order order = new Order();
+		getOrders().add(order);
+		selected = order;//select the new one
+	}
+	
+	@NotifyChange("selected")
+	@Command
+	public void saveOrder(){
+		getService().save(selected);
+	}
+	
+	
+	@NotifyChange({"selected","orders"})
+	@Command
+	public void deleteOrder(){
+		getService().delete(selected);//delete selected
+		getOrders().remove(selected);
+		selected = null; //clean the selected
+	}
+
+	public OrderService getService() {
+		return FakeOrderService.getInstance();
+	}
+	
+	//validators for prompt
+	public Validator getPriceValidator(){
+		return new AbstractValidator(){
+			public void validate(ValidationContext ctx) {
+				Double price = (Double)ctx.getProperty().getValue();
+				if(price==null || price<=0){
+					addInvalidMessage(ctx, "must large than 0");
+				}
+			}
+		};
+	}
+	
+	public Validator getQuantityValidator(){
+		return new AbstractValidator(){
+			public void validate(ValidationContext ctx) {
+				Integer quantity = (Integer)ctx.getProperty().getValue();
+				if(quantity==null || quantity<=0){
+					addInvalidMessage(ctx, "must large than 0");
+				}
+			}
+		};
+	}
+}
