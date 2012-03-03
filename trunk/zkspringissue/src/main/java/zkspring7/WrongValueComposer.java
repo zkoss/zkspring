f0ed4078d150c5b44849ae6e6c5002355630ac2a
hawkchen@potix.com <hawkchen@potix.com@28855f3a-1600-e85e-1cf9-12e366649bb5>
2012-03-03 09:29:29 +0000

    modify reproducing case to generate WrongValuesException and custom run-time exception for contrast.

--- trunk/zkspringissue/src/main/java/zkspring7/WrongValueComposer.java
+++ trunk/zkspringissue/src/main/java/zkspring7/WrongValueComposer.java
@@ -1,15 +1,30 @@
 package zkspring7;
 
 import org.zkoss.zk.ui.WrongValueException;
+import org.zkoss.zk.ui.WrongValuesException;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Textbox;
 
+@SuppressWarnings("serial")
 public class WrongValueComposer extends GenericForwardComposer {
 	
 	private Textbox username;
+	private Textbox password;
 	
-	public void onClick$button(){
+	public void onClick$wveButton(){
 		username.clearErrorMessage();
-		throw new WrongValueException(username, "Not a valid username or password. Please retry.");
+		throw new WrongValueException(username, "Not a valid username.");
+	}
+
+	public void onClick$wvseButton(){
+		username.clearErrorMessage();
+		password.clearErrorMessage();
+		WrongValueException[] wrongValues = {new WrongValueException(username, "Not a valid username."),
+				new WrongValueException(password, "Not a valid password.")};
+		throw new WrongValuesException(wrongValues);
+	}
+	
+	public void onClick$myEx(){
+		throw new RuntimeException(">>>Hawk runtime exception");
 	}
 }
