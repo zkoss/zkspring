97ef9768498491c88a0e23ac0ead4cc633ab4d1d
hawkchen@potix.com <hawkchen@potix.com@28855f3a-1600-e85e-1cf9-12e366649bb5>
2012-12-11 06:26:21 +0000

    add test case "Send event to a detached IdSpace Component with detached parent"

--- trunk/zkspringissue/src/main/java/zkspring22/DetachComponentComposer.java
+++ trunk/zkspringissue/src/main/java/zkspring22/DetachComponentComposer.java
@@ -4,11 +4,19 @@ import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Label;
+import org.zkoss.zul.Window;
 
 public class DetachComponentComposer extends GenericForwardComposer {
 	
 	public void onClick$button(){
 		Label label  =new Label();
 		Events.sendEvent(label, new Event("onTest",label));
+	}
+	
+	public void onClick$button2(){
+		Window inner  =new Window();
+		Window outer = new Window();
+		outer.appendChild(inner);
+		Events.sendEvent(inner, new Event("onTest",inner));
 	}
 }
