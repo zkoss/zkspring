bdb2bd2bc0b2d3f06e1b4eba3eb39c8dbba2d4f4
hawkchen@potix.com <hawkchen@potix.com@28855f3a-1600-e85e-1cf9-12e366649bb5>
2012-12-12 06:22:14 +0000

    initial import

---
+++ webflow-example/src/main/java/example/FlowHelper.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author mschroen
+ */
+public class FlowHelper implements Serializable{
+    
+    
+    private List myList;
+    private boolean mybool;
+    private int myint;
+    
+    String value1 = "A";
+    
+    
+
+    public boolean isMybool() {
+        return mybool;
+    }
+
+    public void setMybool(boolean mybool) {
+        this.mybool = mybool;
+    }
+
+    public int getMyint() {
+        return myint;
+    }
+
+    public void setMyint(int myint) {
+        this.myint = myint;
+    }
+
+    public List getMyList() {
+    	System.out.println("FlowHelper: getMyList:"+myList+", this "+toString());
+//    	new Exception("getMyList").printStackTrace();
+        return myList;
+    }
+
+    public void setMyList(List myList) {
+    	System.out.println("FlowHelper: setMyList:"+myList+", this "+toString());
+//    	new Exception("setMyList").printStackTrace();
+        this.myList = myList;
+    }
+
+    private void readObject(ObjectInputStream ois)
+    		throws IOException, ClassNotFoundException {
+    	ois.defaultReadObject();
+    	System.out.println("read "+ this);
+    }
+
+    private void writeObject(ObjectOutputStream oos)
+    		throws IOException {
+    	oos.defaultWriteObject();
+    	System.out.println("write "+ this);
+    }
+
+	public String getValue1() {
+		return value1;
+	}
+
+	public void setValue1(String value1) {
+		this.value1 = value1;
+		System.out.println(">>>>"+value1);
+	}
+	
+	public void kick(){
+		System.out.println(">>>>Kick me");
+	}
+
+	public void addItem(String item){
+		if(myList!=null){
+			myList.add(item);
+		}
+	}
+}
