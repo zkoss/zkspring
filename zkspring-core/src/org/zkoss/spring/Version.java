bfa6bfa44c26414d01c051b9d3fdca3e911cea45
hawkchen <hawkchen@potix.com>
2018-02-14 16:19:15 +0800

    update version to 4.0.0

--- zkspring-core/src/org/zkoss/spring/Version.java
+++ zkspring-core/src/org/zkoss/spring/Version.java
@@ -28,5 +28,5 @@ package org.zkoss.spring;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "3.2.1-SNAPSHOT";
+	public static final String UID = "4.0.0";
 }
