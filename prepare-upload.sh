b17520e89b8d57dc297b84c33fe96512f18441b8
Robert Wenzel <robertwenzel@potix.com>
2019-01-24 16:44:37 +0800

    include javadocs in buildscript

--- prepare-upload.sh
+++ prepare-upload.sh
@@ -4,16 +4,18 @@ VERSION=$1
 set -e
 
 UPLOAD_FOLDER=./target/to_upload
-MVN_FOLDER=$VERSION/maven
+MVN_FOLDER=maven
 
-ZS_CORE_PATH=$UPLOAD_FOLDER/zkspring-core/$MVN_FOLDER
-ZS_SECURITY_PATH=$UPLOAD_FOLDER/zkspring-security/$MVN_FOLDER
+ZS_CORE_PATH=$UPLOAD_FOLDER/zkspring-core
+ZS_SECURITY_PATH=$UPLOAD_FOLDER/zkspring-security
 
 rm -rf $UPLOAD_FOLDER
 
-mkdir -p $ZS_CORE_PATH
-mkdir -p $ZS_SECURITY_PATH
+mkdir -p $ZS_CORE_PATH/$VERSION/$MVN_FOLDER
+mkdir -p $ZS_SECURITY_PATH/$VERSION/$MVN_FOLDER
 
-cp zkspring-core/target/zkspring-core-$VERSION-bundle.jar $ZS_CORE_PATH
+cp zkspring-core/target/zkspring-core-$VERSION-javadoc.jar $ZS_CORE_PATH/$VERSION/
+cp zkspring-core/target/zkspring-core-$VERSION-bundle.jar $ZS_CORE_PATH/$VERSION/$MVN_FOLDER
 
-cp zkspring-security/target/zkspring-security-$VERSION-bundle.jar $ZS_SECURITY_PATH
+cp zkspring-security/target/zkspring-security-$VERSION-javadoc.jar $ZS_SECURITY_PATH/$VERSION/
+cp zkspring-security/target/zkspring-security-$VERSION-bundle.jar $ZS_SECURITY_PATH/$VERSION/$MVN_FOLDER
