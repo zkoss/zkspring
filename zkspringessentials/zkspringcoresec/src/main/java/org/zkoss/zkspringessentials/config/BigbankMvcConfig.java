6ea6f64e29d3c0638bb69d867766efeebc1a0eff
hawkchen <hawkchen@potix.com>
2024-11-06 16:49:18 +0800

    remove unused class

--- zkspringessentials/zkspringcoresec/src/main/java/org/zkoss/zkspringessentials/config/BigbankMvcConfig.java
+++ zkspringessentials/zkspringcoresec/src/main/java/org/zkoss/zkspringessentials/config/BigbankMvcConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Controller
